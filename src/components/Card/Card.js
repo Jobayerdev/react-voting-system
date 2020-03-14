9c111230ea8ba992ab14bf9edc676c44f23605e2
Jobayer Hossain <Jobayerhossain977@Gmail.com>
2020-03-14 17:42:47 +0600

    done ready to deploy

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./card.style.css";
 import PrimaryTitle from "../Primary-Title/PrimaryTitle";
 import Poll from "../Poll/Poll";
 
 const Card = () => {
+	const [option, setOption] = useState({
+		questions:
+			"If you write a book about failure and it doesn't sell ,is it called success ?",
+		answer: [
+			{ title: "Definitely a Sucess", option: "yes", count: 80 },
+			{ title: "No", option: "no", count: 40 },
+			{
+				title: "Only if it's in the To section of the library",
+				option: "nocomment",
+				count: 50
+			}
+		],
+		totalCount: 190
+	});
+
+	const updateState = (getData) => {
+		const get = option.answer.find((x) => x.option === getData.option);
+		get.count++;
+		const newAdd = [...option.answer, get];
+		const removeDuplicate = newAdd.filter(function(item, index) {
+			return newAdd.indexOf(item) === index;
+		});
+
+		const total = option.answer.reduce(
+			(acc, current) => acc + current.count,
+			0
+		);
+
+		setOption({
+			...option,
+			removeDuplicate,
+			totalCount: total
+		});
+	};
 	return (
 		<div className="card">
-			<PrimaryTitle>
-				If you write a book about failure and it doesn't sell ,is it called
-				success ?
-			</PrimaryTitle>
+			<PrimaryTitle>{option.questions}</PrimaryTitle>
 			<div className="options">
-				<Poll />
+				{option.answer
+					.sort((a, b) => b.count - a.count)
+					.map((x, i) => (
+						<Poll
+							key={i}
+							option={x}
+							totalCount={option.totalCount}
+							changeHandler={updateState}
+						/>
+					))}
 			</div>
 		</div>
 	);
--- src/components/Poll/Poll.js
+++ src/components/Poll/Poll.js
@@ -1,15 +1,30 @@
 import React from "react";
 import "./Poll.style.css";
 
-const Poll = () => {
+const Poll = ({ option, changeHandler, totalCount }) => {
 	return (
 		<div className="poll-item">
-			<h4 className="poll-title">lorem ipsun</h4>
+			<div className="info">
+				<div className="mark">
+					<i onClick={() => changeHandler(option)} className="far fa-heart"></i>
+					<h4 className="poll-title">{option.title}</h4>
+				</div>
+				<div className="progress-bg">
+					<div
+						className={`${option.option === "yes" ? "green" : ""} ${
+							option.option === "no" ? "red" : ""
+						} ${option.option === "nocomment" ? "no-comment" : ""} progress`}
+						style={{
+							width: `${Math.floor((option.count * 100) / totalCount)}%`
+						}}></div>
+				</div>
+			</div>
 			<div className="vote-icon">
-				<i className="far fa-heart"></i>
+				<p>${Math.round((option.count * 100) / totalCount)}%</p>
 			</div>
 		</div>
 	);
 };
 
 export default Poll;
+// Math.round((option.count * 100) / totalCount)
